93c82e0f0f31dbd21cb5133dde394888a385c14e
Julian Matias Bermolen <jbermolen@dlatv.net>
2018-01-30 17:27:23 -0300

    Cerrar sesión

--- AndroidStudioProjects/golapp/app/src/main/java/ar/com/golapp/golapp/MainActivity.java
+++ AndroidStudioProjects/golapp/app/src/main/java/ar/com/golapp/golapp/MainActivity.java
@@ -1,5 +1,6 @@
 package ar.com.golapp.golapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +43,7 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
@@ -91,11 +95,23 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
-
+            signOut();
+            goLoginScreen();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    private void goLoginScreen() {
+        Intent intent = new Intent(this,LoginFirebase.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    public void signOut(){
+        FirebaseAuth.getInstance().signOut();
+    }
+
 }
